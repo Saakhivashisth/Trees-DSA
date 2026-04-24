d99f5ce231c59c518a1b6f4ad42c4627b31704ff
Saakhivashisth <saakhi.vashisth@gmail.com>
2026-04-24 16:01:56 +0530

    Time: 0 ms (100%), Space: 44.8 MB (83.95%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,16 +14,18 @@
  * }
  */
 class Solution {
-
     public boolean hasPathSum(TreeNode root, int targetSum) {
-     if(root==null){
-        return false;
-     }
-     if(root.right==null&&root.left==null){
-        return targetSum==root.val;
-     }
+        if(root==null) return false;
 
-     return hasPathSum(root.left,targetSum-root.val)|| hasPathSum(root.right,targetSum-root.val);
+        if(root.left==null&& root.right==null){
+            return targetSum==root.val;
+        }
+
+        int rem=targetSum-root.val;
+       return hasPathSum(root.left,rem)||hasPathSum(root.right,rem);
+        
+
 
     }
+    
 }
